12fef12886f4a36c315c867f5f0864f318384e0c
Alexandr Andrianov <alexandr.andrianov@rdtex.ru>
2011-09-29 16:51:20 +0400

    init

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_scrumbler do
+  name 'Redmine Scrumbler plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
