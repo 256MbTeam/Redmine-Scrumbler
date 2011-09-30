232b850a0bf247f7f64848359c3622c049150cb2
Alexandr Andrianov <alexandr.andrianov@rdtex.ru>
2011-09-30 15:54:42 +0400

    clean

--- app/controllers/scrumbler_settings_controller.rb
+++ app/controllers/scrumbler_settings_controller.rb
@@ -1,15 +1,20 @@
 class ScrumblerSettingsController < ScrumblerAbstractController
   unloadable
 
-#  before_filter :authorize, :only => [:settings]
+  #  before_filter :authorize, :only => [:settings]
   
   def show
   end
   
   def update_maintrackers
     params[:scrumbler_maintrackers] ||= []
+    ScrumblerProjectSetting.transaction do
+      unless @scrumbler_project_setting.update_attributes(:maintrackers => params[:scrumbler_maintrackers].map(&:to_i))
+        flash[:error] = t :error_scrumbler_maintrackers_update
+      end
+    end
     
-    @scrumbler_project_setting.update_attributes(:maintrackers => params[:scrumbler_maintrackers].map(&:to_i))
+    flash[:notice] = t :notice_successful_update unless flash[:error]
     redirect_to project_scrumbler_settings_url(@project)
   end
   
