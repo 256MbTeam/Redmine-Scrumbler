77751fc84ef06ef2c34368c60331ff2d02ab8dba
Alexandr Andrianov <alexandr.andrianov@rdtex.ru>
2011-09-30 16:08:26 +0400

    add sprints

--- app/models/scrumbler_sprint.rb
+++ app/models/scrumbler_sprint.rb
@@ -1,9 +1,15 @@
 class ScrumblerSprint < ActiveRecord::Base
   unloadable
   
+  default_scope :include => [:version]
+  
   belongs_to :project
   belongs_to :version
   
   has_many :scrumbler_sprint_trackers
   has_many :scrumbler_sprint_statuses
+  
+  delegate :name, :to => :version
+  
+  
 end
