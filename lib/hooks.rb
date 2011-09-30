8177d29ec7f205d012b74c087df504ad4ef9ca55
Ivan Kotenko <Ivan.Kotenko@rd-consult.ru>
2011-09-30 19:45:02 +0400

    remove puts

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -1,13 +1,11 @@
 module Scrumbler
   class Hooks < Redmine::Hook::ViewListener
     def destroy_version(params)
-      puts "destroy_version(params)\n"*8
       @version = params[:version]
       ScrumblerSprint.destroy_if_exists(@version.project_id, @version.id)
     end
     
     def create_version(params)
-      puts "create_version(params)\n"*8
       @version = params[:version]
       ScrumblerSprint.crate_if_not_exists(@version.project_id, @version.id)
     end
