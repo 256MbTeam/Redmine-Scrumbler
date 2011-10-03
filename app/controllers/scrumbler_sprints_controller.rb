aad7e3ceb56d6c7501cef0c5fdff824bfdc99139
Ivan Kotenko <Ivan.Kotenko@rd-consult.ru>
2011-10-03 20:32:32 +0400

    add license in controller. change comments

--- app/controllers/scrumbler_sprints_controller.rb
+++ app/controllers/scrumbler_sprints_controller.rb
@@ -1,3 +1,20 @@
+# Scrumbler - Add scrum functionality to any Redmine installation
+# Copyright (C) 2011 256Mb Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 class ScrumblerSprintsController < ScrumblerAbstractController
   unloadable
 
@@ -11,22 +28,22 @@ class ScrumblerSprintsController < ScrumblerAbstractController
   end
   
   def update_trackers
-    # Удаление пустых данных
+    # delete empty data
     params[:scrumbler_sprint][:scrumbler_sprint_trackers].delete_if { |obj|  !obj[:tracker_id]}
     
-    # Выборка ID существующих трекеров
+    # select existing trackers id
     @existing_sprint_trackers_ids = @scrumbler_sprint.scrumbler_sprint_trackers.map &:tracker_id
     
-    # Выборка ID трекеров из параметров
+    # trackers id from params
     @new_sprint_trackers_ids =  params[:scrumbler_sprint][:scrumbler_sprint_trackers].map {|obj| obj[:tracker_id].to_i}
 
-    # Выборка трекеров для создания
+    # select trackers for create
     @to_create_trackers = params[:scrumbler_sprint][:scrumbler_sprint_trackers].find_all { |e| !@existing_sprint_trackers_ids.include? e[:tracker_id].to_i }
     
-    # Выборка трекеров для обновления
+    # select trackers for update
     @to_update_trackers = params[:scrumbler_sprint][:scrumbler_sprint_trackers] - @to_create_trackers
     
-    # Выборка трекеров для удаления
+    # select trackers for destroy
     @to_destroy_trackers_ids = @existing_sprint_trackers_ids - @new_sprint_trackers_ids
 
     ScrumblerSprintTracker.transaction do
@@ -58,13 +75,13 @@ class ScrumblerSprintsController < ScrumblerAbstractController
   end
   
   def update_issue_statuses
-    @scrumbler_issue_statuses_ids = params[:scrumbler_issue_statuses].map(&:to_i)
+    @scrumbler_issue_statuses = params[:scrumbler_issue_statuses].map(&:to_i)
     
     @enabled_issue_statuses = @scrumbler_sprint.scrumbler_sprint_statuses.map(&:issue_status_id)
        
-    @to_create_issue_statuses = @scrumbler_issue_statuses_ids.find_all { |e| !@enabled_issue_statuses.include? e.to_i }
+    @to_create_issue_statuses = @scrumbler_issue_statuses.find_all { |e| !@enabled_issue_statuses.include? e.to_i }
     
-    @to_destroy_issue_statuses = @enabled_issue_statuses - @scrumbler_issue_statuses_ids
+    @to_destroy_issue_statuses = @enabled_issue_statuses - @scrumbler_issue_statuses
     
     ScrumblerSprintStatus.transaction do
       begin
