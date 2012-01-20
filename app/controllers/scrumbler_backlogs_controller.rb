0c3562f29ef44ae61dc8a86f254c2eacc06b908e
Ivan Kotenko <Ivan.Kotenko@rd-consult.ru>
2012-01-20 18:08:01 +0400

    create basic dnd functionality

--- app/controllers/scrumbler_backlogs_controller.rb
+++ app/controllers/scrumbler_backlogs_controller.rb
@@ -1,10 +1,16 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
-
 class ScrumblerBacklogsController < ScrumblerAbstractController
   unloadable
   def index
-    
-    
+
+  end
+
+  def add_issue_to_sprint
+
+  end
+
+  def remove_issue_from_sprint
+
   end
 end
