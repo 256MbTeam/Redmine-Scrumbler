a80e93c286fe41b075774da27dd879a5427adcb8
Alexandr Andrianov <alexandr.andrianov@rdtex.ru>
2012-02-14 16:38:40 +0400

    sqlite3 hack

---
+++ lib/sqlite_3_serialization_hack.rb
@@ -0,0 +1,30 @@
+# Scrumbler - Add scrum functionality to any Redmine installation
+# Copyright (C) 2011 256Mb Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+# Override binary_to_string SQLite adapter method for supporting hash serialization
+if ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+   ActiveRecord::ConnectionAdapters::SQLiteColumn.module_eval {
+        class << self
+          alias_method :old_binary_to_string, :binary_to_string
+            def binary_to_string(value)
+              return value if value.instance_of? HashWithIndifferentAccess
+              old_binary_to_string(value)
+            end
+        end
+      }
+end
