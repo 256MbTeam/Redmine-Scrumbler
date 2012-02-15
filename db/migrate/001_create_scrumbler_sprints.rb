ab59fea2ea4c526c8c9a6c61b9180603b7e0b370
Alexandr Andrianov <alexandr.andrianov@rdtex.ru>
2012-02-15 16:36:15 +0400

    deleted deprecated migrations

--- db/migrate/001_create_scrumbler_sprints.rb
+++ db/migrate/001_create_scrumbler_sprints.rb
@@ -1,9 +1,9 @@
 class CreateScrumblerSprints < ActiveRecord::Migration
   def self.up
     return if Scrumbler::Migration.migration_exist? "20110929130914-redmine_scrumbler",
-    "20111018172706-redmine_scrumbler",
-    "20111018172707-redmine_scrumbler",
-    "20111018172704-redmine_scrumbler"  
+                                                    "20111018172706-redmine_scrumbler",
+                                                    "20111018172707-redmine_scrumbler",
+                                                    "20111018172704-redmine_scrumbler"  
     
     
     create_table :scrumbler_sprints, :force => true do |t|
@@ -17,6 +17,8 @@ class CreateScrumblerSprints < ActiveRecord::Migration
   end
 
   def self.down
+    ScrumblerIssueCustomField.points.destroy
     drop_table :scrumbler_sprints
+    
   end
 end
--- db/migrate/002_create_scrumbler_project_settings.rb
+++ db/migrate/002_create_scrumbler_project_settings.rb
@@ -1,7 +1,7 @@
 class CreateScrumblerProjectSettings < ActiveRecord::Migration
   def self.up
     return if Scrumbler::Migration.migration_exist? "20110930101049-redmine_scrumbler",
-    "20111018172704-redmine_scrumbler"  
+                                                    "20111018172704-redmine_scrumbler"  
     
     create_table :scrumbler_project_settings, :force => true do |t|
       t.references :project
