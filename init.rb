f89f8113912fdbc31e391f87ed32a5cabbd1218b
Pedro Medeiros <pedro@pemedeiros.com>
2012-06-15 09:08:07 -0300

    remove dispatcher library

--- init.rb
+++ init.rb
@@ -16,7 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 require 'sqlite_3_serialization_hack'
 require 'redmine'
-require 'dispatcher'
+#require 'dispatcher'
 
 require_dependency "scrumbler"
 
